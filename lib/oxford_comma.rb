36e906b40d1ff7aaf53470808a75be8b3349172e
Ryan Hollins <ballbaroi@gmail.com>
2020-02-17 14:36:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 def oxford_comma(array)
-  ["k","i","w","i"].join
-end
-
-["kiwi,""durian"].join("and")
-end
+  if array.size == 1
+    return array[0]
+    elsif array.size == 2
+    return array.join(" and ")
+  else
+    return array[0..-2].join??????
